75e434b2aa374999fafa938801e85d9a3b76e0dc
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-07-29 16:54:47 +0200

    Renderet en 2D trekant
    Kodet videre, og har nu en render, som kan arbejde med 2D primitives

--- OpenGl_Rendere/Main.cpp
+++ OpenGl_Rendere/Main.cpp
@@ -2,33 +2,119 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-int main() {
-	glfwInit(); //init glfw
+//Laver værdier der indenholder Vertex, og Fragment shaders. gemmes i en Vector 4 værdi.
+//er ikke helt sikker dog
+const char* vertexShaderSource = "#version 330 core \n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n" "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" "}\0";
 
+const char* fragmentShaderSource = "#version 330 core \n" "out vec4 FragColor;\n"
+"void main()\n"
+"{\n" "FragColor = vec4(0.8f,0.3f,0.2f,1.0f);\n" "}\n\0";
+
+int main() {
+
+	//init glfw
+	glfwInit();
+
+	//fortæller glfw hvilke verion den skal bruge
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//kordinater til alle vertices... trekanter og matematik. alle kordinater virker, så længe det er mellem 1, og -1.
+	GLfloat vertices[] = {
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+	};
+
+	//specifere indstillinger, af et vindue
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Render", NULL, NULL);
-	if (window == NULL) { //hvis window ikke kan laves, returnere den fejl.. i stedet for at fortsætte, og crashe.
+
+	//hvis window ikke kan laves, returnere den fejl.. i stedet for at fortsætte, og crashe.
+	if (window == NULL) { 
 		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	glfwMakeContextCurrent(window); //Fortæller Context at lave "window"
-	gladLoadGL(); //init openGL
+	//Fortæller Context at lave "window"
+	glfwMakeContextCurrent(window);
 
-	glViewport(0, 0, 800, 800); //Fortæller glad, hvormeget den stal tegne på.
+	//init openGL
+	gladLoadGL();
 
-	glClearColor(0.61f, 0.07f, 0.12f, 1.0f); //Angiver farve
+	//Fortæller glad, hvormeget den stal tegne på.
+	glViewport(0, 0, 800, 800);
 
-	glClear(GL_COLOR_BUFFER_BIT); //Putter farven ind i back buffer
-	glfwSwapBuffers(window); //bytter front og back buffer.
+	//Fortæller GPU at behandle shader data med en vertex shader. Compiler data til MachineLangues, der GPU ikke taler menneske kode hehe.
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
+	//Fortæller GPU at behandle shader data med en fragment shader.
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//Sender data i store pakker, igennem en buffer. Dette data skal "bind" så det gør vi her
+	GLuint VAO, VBO;
+
+	//VAO skal før VBO
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	//Angiver farve
+	glClearColor(0.61f, 0.07f, 0.12f, 1.0f);
+
+	//Putter farven ind i back buffer
+	glClear(GL_COLOR_BUFFER_BIT); 
+
+	//bytter front og back buffer.
+	glfwSwapBuffers(window); 
+
+	//Fortæller glfw at den skal updatere events, når de er klar.
 	while(!glfwWindowShouldClose(window)) {
-		glfwPollEvents(); //Fortæller glfw at den skal updatere events, når de er klar.
+
+		//Tegner 2D primitiven
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
