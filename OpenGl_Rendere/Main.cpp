3fb492caef9e5024076b0855412b9b08cf36b91f
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-07-30 18:52:31 +0200

    Oprydning i kode.
    VBO VAO og EBO, har fået sine egne filer, og klasser.

--- OpenGl_Rendere/Main.cpp
+++ OpenGl_Rendere/Main.cpp
@@ -2,6 +2,30 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#include "shaderClass.h" //Farver
+#include "VAO.h"		//VertexArrayObject
+#include "VBO.h"	   //VertexBufferObject
+#include "EBO.h"	  //IndiciesBufferObject??
+
+
+GLfloat vertices[] = {
+//    -X-                  -Y-
+	-0.5f,      -0.5f  * float(sqrt(3))     / 3,  0.0f,   //Bund venstre hjørne
+	 0.5f,      -0.5f  * float(sqrt(3))     / 3,  0.0f,  //Bund højer hjørne
+	 0.0f,       0.5f  * float(sqrt(3)) * 2 / 3,  0.0f, // Top Midt hjørne
+
+    -0.5f / 2,   0.05f * float(sqrt(3))     / 6,  0.0f,  //Mid venstre hjørne
+	 0.5f / 2,   0.05f * float(sqrt(3))     / 6,  0.0f, //Mid Højer Hjørne
+	 0.0f,      -0.05f * float(sqrt(3))     / 3,  0.0f // Mid Bund hjørne
+};
+
+GLuint indices[] = {
+
+	0, 3, 5,  //Bund Venstre trekant
+	3, 2, 4, //Bund Højer trekant
+	5, 4, 1 //Top trekant
+};
+
 int main() {
 
 	//init glfw
@@ -12,18 +36,11 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//kordinater til alle vertices... trekanter og matematik. alle kordinater virker, så længe det er mellem 1, og -1.
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
-	};
-
 	//specifere indstillinger, af et vindue
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Render", NULL, NULL);
 
 	//hvis window ikke kan laves, returnere den fejl.. i stedet for at fortsætte, og crashe.
-	if (window == NULL) { 
+	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -38,72 +55,44 @@ int main() {
 	//Fortæller glad, hvormeget den stal tegne på.
 	glViewport(0, 0, 800, 800);
 
-	//Fortæller GPU at behandle shader data med en vertex shader. Compiler data til MachineLangues, der GPU ikke taler menneske kode hehe.
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	Shader shaderProgram("default.vert", "default.frag");
 
-	//Fortæller GPU at behandle shader data med en fragment shader.
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	
-	GLuint shaderProgram = glCreateProgram();
+	VAO VAO1;
+	VAO1.Bind();
 
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
 
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	//Sender data i store pakker, igennem en buffer. Dette data skal "bind" så det gør vi her
-	GLuint VAO, VBO;
-
-	//VAO skal før VBO
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-
-	//Angiver farve
-	glClearColor(0.61f, 0.07f, 0.12f, 1.0f);
-
-	//Putter farven ind i back buffer
-	glClear(GL_COLOR_BUFFER_BIT); 
-
-	//bytter front og back buffer.
-	glfwSwapBuffers(window); 
+	VAO1.LinkVBO(VBO1, 0);
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
 	//Fortæller glfw at den skal updatere events, når de er klar.
-	while(!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window)) {
 
 		//Tegner 2D primitiven
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		shaderProgram.Activate();
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+		//Skifer usynlige, og synlige buffers
 		glfwSwapBuffers(window);
 
+		//Pooler events up, så alt er tegnet inden det bliver vist
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
+	//fjerner alt unødvendige data, der er blevet forarbejdet, eller binded
 
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+
+	//lukker vinduet
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
--- OpenGl_Rendere/shaderClass.cpp
+++ OpenGl_Rendere/shaderClass.cpp
@@ -1,6 +1,8 @@
 #include "shaderClass.h"
 
 std::string get_file_contents(const char* filename) {
+
+	std::ifstream in(filename, std::ios::binary);
 	if (in) {
 
 		std::string contents;
@@ -13,3 +15,42 @@ std::string get_file_contents(const char* filename) {
 	}
 	throw(errno);
 }
+
+Shader::Shader(const char* vertexFile, const char* fragmentFile)
+{
+	std::string vertexCode = get_file_contents(vertexFile);
+	std::string fragmentCode = get_file_contents(fragmentFile);
+
+	const char* vertexSource = vertexCode.c_str();
+	const char* fragmentSource = fragmentCode.c_str();
+
+	//Fortæller GPU at behandle shader data med en vertex shader. Compiler data til MachineLangues, der GPU ikke taler menneske kode hehe.
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Fortæller GPU at behandle shader data med en fragment shader.
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	ID = glCreateProgram();
+
+	glAttachShader(ID, vertexShader);
+	glAttachShader(ID, fragmentShader);
+
+	glLinkProgram(ID);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+void Shader::Activate() {
+
+	glUseProgram(ID);
+};
+
+void Shader::Delete() {
+
+	glDeleteProgram(ID);
+};
