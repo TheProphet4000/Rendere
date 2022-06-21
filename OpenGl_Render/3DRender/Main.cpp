7951b0b4266059669a7f5bee2c693100cd6000f2
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2022-06-21 15:09:36 +0200

    Works

--- OpenGl_Render/3DRender/Main.cpp
+++ OpenGl_Render/3DRender/Main.cpp
@@ -1,4 +1,3 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -17,4 +16,20 @@ int main() {
 	}
 
 	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # Rendere
 
 My little render project, where my goal is to make a 3D render (maybe a 2D too) with OpenGL.
-Using VisualStudio as IDEA, with the aid of Glad and GLFW liberies.
+Using VisualStudio with GLFW liberies.
 
 Written in C++
 runs 64bit
-
-Note: Due to GLFW being a libery tailored for your OS and PC, you'll need to build your own libery. This can be done with Cmake.
-	 Remember to get a GLAD.c files and add it to your root folder.
